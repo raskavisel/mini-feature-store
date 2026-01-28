e04662ec770fb7fbfb868de2423cbe48124b8894
raskavisel <118968076+raskavisel@users.noreply.github.com>
2026-01-28 08:29:44 +0530

    Update version arguments to integers in add_feature

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ print("Starting Feature Store...")
 database.connect_db()
 
 # Add some features
-database.add_feature("user_1", "age", 25, version="1")
-database.add_feature("user_1", "age", 26, version="2")
-database.add_feature("user_1", "income", 50000, version="1")
+database.add_feature("user_1", "age", 25, version=1)
+database.add_feature("user_1", "age", 26, version=2)
+database.add_feature("user_1", "income", 50000, version=1)
 
 # Retrieve features
 print("Latest 'age' for user_1:", database.get_feature("user_1", "age"))
