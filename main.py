3974a5f9be258339da8e3a7c4b9700fb30656f31
raskavisel <118968076+raskavisel@users.noreply.github.com>
2026-01-31 04:20:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,5 +12,5 @@ database.add_feature("user_1", "income", 50000, version=1)
 
 # Retrieve features
 print("Latest 'age' for user_1:", database.get_feature("user_1", "age"))
-print("Version 1 'age' for user_1:", database.get_feature("user_1", "age", version="1"))
+print("Version 1 'age' for user_1:", database.get_feature("user_1", "age", version=1))
 print("Income for user_1:", database.get_feature("user_1", "income"))
